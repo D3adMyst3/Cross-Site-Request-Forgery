19be88e02d68cdf26b14f6f5ab4bd2f4dbb41ff5
D3adMyst3 <55168622+D3adMyst3@users.noreply.github.com>
2019-10-13 13:36:59 +0530

    Add files via upload

---
+++ DSC/index.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title> CSRF protection</title>
+       
+		<link rel="stylesheet" href="index.css">
+		<meta charset="UTF-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  		<link href="https://fonts.googleapis.com/css?family=Pacifico|Roboto&display=swap" rel="stylesheet">
+ 		<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+  		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	</head>
+	<body>
+		<div class="main">		
+		<form>
+			<div align='center'>
+				<a href="signin.php" class="button">CSRF protection by double submit cookies</a>
+			</div>
+		</form>
+		</div>
+	</body>
+</html>
